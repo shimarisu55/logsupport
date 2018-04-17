3938069a20a41e629fd5e99ed40b3641501707a3
yukidaru55 <shiho.2480@gmail.com>
2018-04-17 12:33:56 +0000

    paranoia

--- db/migrate/20180417122309_change_deleted_at_to_users.rb
+++ db/migrate/20180417122309_change_deleted_at_to_users.rb
@@ -1,6 +1,6 @@
 class ChangeDeletedAtToUsers < ActiveRecord::Migration[5.1]
   def change
-  		change_column :Users, :deleted_at, :datetime
+  		change_column :Users, :deleted_at, :datetime, default: nil
   end
 
 end
